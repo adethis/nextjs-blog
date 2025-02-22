8c05fe1607ddae417e6f10fd7edab768ef11af32
adetrihadi <adetrihadi@jatelindo.co.id>
2025-02-22 22:04:39 +0700

    update: intros at index.js

--- pages/index.js
+++ pages/index.js
@@ -22,7 +22,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Introduction]</p>
+        <p>Hello Peppp's</p>
         <p>
           (This is a sample website - you'll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>. )
